93ea9db67f94e92e686519c531281845e6383395
Pavel Sadykov <2439747@gmail.com>
2023-04-02 12:18:56 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,124 @@
+//
+//  main.cpp
+//  UML ДЗ 2 Мост и Компоновщик
+//
+//  Created by Павел on 02.04.2023.
+//
+#include <iostream>
+#include <vector>
+
+// Transport.h
+class Transport {
+public:
+    virtual void loadGoods() = 0;
+};
+//класс самосвал для перевозки сыпучих материалов
+class DumpTruck : public Transport {
+public:
+    void loadGoods() override {
+        std::cout << "Unloading bulk material from a Dumptruck" << std::endl;
+    }
+};
+//класс автомобиль для перевозки мелких грузов
+class Car : public Transport {
+public:
+    void loadGoods() override {
+        std::cout << "Unloading goods from car" << std::endl;
+    }
+};
+
+//класс бензовоз для перевозки топлива
+class FuelTanker : public Transport {
+public:
+    void loadGoods() override {
+        std::cout << "Unloading Fuel from a FuelTanker" << std::endl;
+    }
+};
+
+
+// Storage.h
+class Storage {
+public:
+    Storage(Transport* transport) : transport_(transport) {}
+    virtual void receiveGoods() = 0;
+    virtual void storeGoods() = 0;
+protected:
+    Transport* transport_;
+};
+//класс склад для хранения сыпучих материалов
+class Warehouse : public Storage {
+public:
+    Warehouse(Transport* transport) : Storage(transport) {}
+    void receiveGoods() override {
+        transport_->loadGoods();
+    }
+    void storeGoods() override {
+        std::cout << "Storing bulk material in warehouse" << std::endl;
+    }
+};
+//класс гараж для хранения мелких грузов
+class Garage : public Storage {
+public:
+    Garage(Transport* transport) : Storage(transport) {}
+    void receiveGoods() override {
+        transport_->loadGoods();
+    }
+    void storeGoods() override {
+        std::cout << "Storing goods in garage" << std::endl;
+    }
+};
+//класс резервуар для хранения топлива
+class StorageTank : public Storage {
+public:
+    StorageTank(Transport* transport) : Storage(transport) {}
+    void receiveGoods() override {
+        transport_->loadGoods();
+    }
+    void storeGoods() override {
+        std::cout << "Storing Fuel in tank" << std::endl;
+    }
+};
+
+
+// StorageComposite.h
+
+class StorageComposite : public Storage {
+public:
+    StorageComposite(Transport* transport) : Storage(transport) {}
+    void add(Storage* storage) {
+        storages_.push_back(storage);
+    }
+    void receiveGoods() override {
+        for (Storage* storage : storages_) {
+            storage->receiveGoods();
+        }
+    }
+    void storeGoods() override {
+        for (Storage* storage : storages_) {
+            storage->storeGoods();
+        }
+    }
+private:
+    std::vector<Storage*> storages_;
+};
+
+
+
+int main(int argc, const char * argv[]) {
+    
+        Transport* truck = new DumpTruck ();
+        Transport* car = new Car();
+        Transport* tanker = new FuelTanker ();
+        Storage* warehouse = new Warehouse(truck);
+        Storage* garage = new Garage(car);
+        Storage* storagetank = new StorageTank(tanker);
+        StorageComposite* composite = new StorageComposite(truck);
+        composite->add(warehouse);
+        composite->add(garage);
+        composite->add(storagetank);
+        composite->receiveGoods();
+        composite->storeGoods();
+       
+
+    return 0;
+}
